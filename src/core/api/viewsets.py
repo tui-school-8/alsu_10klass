510f887a26c43bae7257a847893a943928085e54
Nail Gimaev <gimntut@list.ru>
2020-03-01 22:09:08 +0500

    Models

---
+++ src/core/api/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework.viewsets import ModelViewSet
+
+
+class CardViewSet(ModelViewSet):
+    serializer_class = None
+
+
+class PollViewSet(ModelViewSet):
+    serializer_class = None
+
+
+class ComparisionViewSet(ModelViewSet):
+    serializer_class = None
